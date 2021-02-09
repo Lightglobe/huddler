fd0ca58346d017acbb28594b12f5d4a61371c75f
Lightglobe <alextsambalos@gmail.com>
2021-02-09 19:25:55 +0200

    Updated PhotoPage template

--- src/features/user/Settings/PhotosPage.js
+++ src/features/user/Settings/PhotosPage.js
@@ -1,5 +1,14 @@
 import React from "react";
-import { Box, Heading, Tabs, Tab, Image, Carousel, Button } from "grommet";
+import {
+  Box,
+  Heading,
+  Tabs,
+  Tab,
+  Image,
+  Carousel,
+  Text,
+  Anchor,
+} from "grommet";
 
 const PhotosPage = () => {
   return (
@@ -19,16 +28,63 @@ const PhotosPage = () => {
               <Image src="/assets/user.png" style={{ width: "300px" }} />
               <Image src="/assets/user.png" style={{ width: "300px" }} />
             </Carousel>
-            <Button alignSelf="center" margin="large">
-              Upload a photo
-            </Button>
+            <Anchor
+              className="link__text"
+              alignSelf="center"
+              margin="medium"
+              style={{ width: "200px" }}
+            >
+              <div className="border__gradient button">
+                <Text size="small" margin={{ left: "5px" }} weight="bold">
+                  Upload Photo
+                </Text>
+              </div>
+            </Anchor>
           </Box>
         </Tab>
         <Tab plain margin="medium" title="Step 2 - Resize Image">
-          <Box pad="medium">Two</Box>
+          <Box alignSelf="center" alignContent="center">
+            <Image
+              margin="medium"
+              alignSelf="center"
+              src="/assets/user.png"
+              style={{ width: "300px" }}
+            />
+            <Anchor
+              className="link__text"
+              alignSelf="center"
+              margin="medium"
+              style={{ width: "200px" }}
+            >
+              <div className="border__gradient button">
+                <Text size="small" margin={{ left: "5px" }} weight="bold">
+                  Resize image
+                </Text>
+              </div>
+            </Anchor>
+          </Box>
         </Tab>
         <Tab plain margin="medium" title="Step 3 - Preview and upload">
-          <Box pad="medium">three</Box>
+          <Box alignSelf="center" alignContent="center">
+            <Image
+              margin="medium"
+              alignSelf="center"
+              src="/assets/user.png"
+              style={{ width: "300px" }}
+            />
+            <Anchor
+              className="link__text"
+              alignSelf="center"
+              margin="medium"
+              style={{ width: "200px" }}
+            >
+              <div className="border__gradient button">
+                <Text size="small" margin={{ left: "5px" }} weight="bold">
+                  Upload
+                </Text>
+              </div>
+            </Anchor>
+          </Box>
         </Tab>
       </Tabs>
     </Box>
